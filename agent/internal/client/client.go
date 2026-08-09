26f1f9c5239fb31ace06b445098fd4aab2286f28
Bansi Dobariya <bansidobariya2510@gmail.com>
2026-08-09 18:03:35 +0100

    feat: establish HTTP request-response tunnel flow

--- agent/internal/client/client.go
+++ agent/internal/client/client.go
@@ -1,16 +1,19 @@
 package client
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gorilla/websocket"
 
+	"encoding/json"
 	"relaygo/shared/protocol"
 )
 
 type Client struct {
 	relayURL string
 	conn     *websocket.Conn
+	tID      string
 }
 
 func NewClient(relayURL string) *Client {
@@ -59,6 +62,13 @@ func (c *Client) Read() error {
 		switch frame.Type {
 		case protocol.Registered:
 			log.Println("Agent registered successfully")
+			var id string
+
+			if err := json.Unmarshal(frame.Payload, &id); err != nil {
+				return err
+			}
+			c.tID = id
+			log.Printf(id)
 
 		case protocol.Ping:
 			log.Println("Got PING, sending PONG")
@@ -70,10 +80,44 @@ func (c *Client) Read() error {
 				return err
 			}
 			log.Println("PONG sent")
+		case protocol.HTTPReqFrame:
+			fmt.Println("got req fram")
+			var req protocol.HTTPReq
+
+			if err := json.Unmarshal(frame.Payload, &req); err != nil {
+				return err
+
+			}
+			log.Printf("HTTP request received: %s %s", req.Method, req.Path)
+			res := protocol.HTTPRes{
+				ID:     req.ID,
+				Status: 200,
+				Header: map[string]string{},
+				Body:   []byte("Hello from agent"),
+			}
+			payload, err := json.Marshal(res)
+			if err != nil {
+				return err
+			}
+			if err := c.WriteFrame(protocol.Frame{
+				Type:    protocol.HTTPResFrame,
+				Payload: payload,
+			}); err != nil {
+				return err
+			}
+
 		}
 	}
 
 	//return nil
+}
+func (c *Client) WriteFrame(frame protocol.Frame) error {
+	data, err := protocol.Encode(frame)
+	if err != nil {
+		return err
+	}
+
+	return c.conn.WriteMessage(websocket.TextMessage, data)
 }
 func (c *Client) Close() error {
 	if c.conn != nil {
